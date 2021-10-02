9b17f66bfb95099fef240e7d292170a9453381a5
Gossa Hernan <hernanagustin.g24@hotmail.com>
2021-10-02 03:48:28 -0300

    First level preview

---
+++ Assets/Scripts/UnstableFloor/UnestableFloorBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class UnestableFloorBehaviour : MonoBehaviour
+{
+    [SerializeField] LayerMask playerMask;
+    [SerializeField] float waitToDestroy;
+    [SerializeField] UnityEvent destroyEvent;
+
+    private void OnCollisionEnter(Collision other) 
+    {
+        if(Contains(playerMask, other.gameObject.layer))
+        {
+            StartCoroutine(InDestroy());
+        }
+    }
+
+    bool Contains(LayerMask mask, int layer)
+    {
+        return mask == (mask | (1 << layer));
+    }
+
+    IEnumerator InDestroy()
+    {
+        yield return new WaitForSeconds(waitToDestroy);
+        //
+        destroyEvent.Invoke();
+    }
+}
