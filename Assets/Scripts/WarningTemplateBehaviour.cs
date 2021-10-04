d0b09d3e1d0fed77ab015add7325db6fcb3973d5
Gossa Hernan <hernanagustin.g24@hotmail.com>
2021-10-04 18:50:24 -0300

    todo xD

---
+++ Assets/Scripts/WarningTemplateBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WarningTemplateBehaviour : MonoBehaviour
+{
+    [SerializeField] Image panel;
+
+    IEnumerator Start()
+    {
+        float value;
+        
+
+        yield return null;
+    }
+}
