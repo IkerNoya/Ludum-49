a269193e412b46677d2eff28a7b07f08ff71c540
Gossa Hernan <hernanagustin.g24@hotmail.com>
2021-10-04 04:08:44 -0300

    Added the rise of the world boundary

---
+++ Assets/Scripts/WorldLimit/FinishTriggerBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class FinishTriggerBehaviour : MonoBehaviour
+{
+    [SerializeField] LayerMask playerMask;
+    [SerializeField] UnityEvent finishTrigger;
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if(Contains(playerMask, other.gameObject.layer))
+        {
+            finishTrigger?.Invoke();
+            Destroy(gameObject);
+        }
+    }
+
+    bool Contains(LayerMask mask, int layer)
+    {
+        return mask == (mask | (1 << layer));
+    }
+}
--- Assets/Scripts/WorldLimit/WorldLimitBehaviour.cs
+++ Assets/Scripts/WorldLimit/WorldLimitBehaviour.cs
@@ -7,17 +7,30 @@ public class WorldLimitBehaviour : MonoBehaviour
     [SerializeField] Transform target;
     [SerializeField] float yOffSet;
     [SerializeField] LayerMask playerMask;
+    [SerializeField] float velocity;
     float lastMaxYPosition;
+    bool moveForwardState;
 
     private void FixedUpdate() 
     {
+
         if(target.position.y > lastMaxYPosition)
             lastMaxYPosition = target.position.y;
         //
-        transform.position = new Vector3(
+        if(moveForwardState)
+        {
+            transform.position += new Vector3(
+                            0,
+                            velocity,
+                            0
+                            );
+        }
+        else
+            transform.position = new Vector3(
                             target.position.x,
                             lastMaxYPosition - yOffSet,
-                            target.position.z);
+                            target.position.z
+                            );
     }
 
     private void OnTriggerEnter(Collider other) 
@@ -35,5 +48,10 @@ public class WorldLimitBehaviour : MonoBehaviour
     bool Contains(LayerMask mask, int layer)
     {
         return mask == (mask | (1 << layer));
+    }
+
+    public void SwitchMoveForwardState()
+    {
+        moveForwardState = !moveForwardState;
     }
 }
